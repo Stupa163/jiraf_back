75f5d25e84bb2e3f30d5e2d124b68c98887b12dc
Remi Bosgaerd <a>
2020-03-13 12:54:11 +0100

    can't register if email is already used

--- routes/register.js
+++ routes/register.js
@@ -2,8 +2,9 @@ const express = require('express');
 const jwt = require('jsonwebtoken');
 const HttpManager = require('../manager/HttpManager');
 const RegisterManager = require('../manager/RegisterManager');
-
+const Models = require('../models');
 const router = express.Router();
+const EmailAlreadyUserError = require('../error/Registration/EmailAlreadyUserError');
 
 /* GET home page. */
 router.post('/', async (req, res) => {
@@ -11,28 +12,36 @@ router.post('/', async (req, res) => {
     try {
         HttpManager.hasRequiredFields(body, 'firstName', 'lastName', 'mail', 'plainPassword', 'phone', 'profile');
         HttpManager.hasRequiredFields(body, 'companyName', 'companySiret', 'companyStatus');
-        const company = RegisterManager.createCompany(body.companyName, body.companySiret, body.companyStatus);
-        const savedCompany = await company.save();
+        let currentUser = await Models.User.findOne({
+            where: {mail: body.mail}
+        });
 
-        const user = await RegisterManager.createUser(
-            body.firstName,
-            body.lastName,
-            body.plainPassword,
-            body.mail,
-            body.phone,
-            body.profile,
-            savedCompany.id,
-        );
-        const savedUser = await user.save();
+        if (currentUser === null) {
+            const company = RegisterManager.createCompany(body.companyName, body.companySiret, body.companyStatus);
+            const savedCompany = await company.save();
 
-        const token = jwt.sign(
-            { userId: savedUser.id },
-            process.env.RANDOM_TOKEN_SECRET,
-            { expiresIn: process.env.TOKEN_EXPIRATION_DELAY },
-        );
-        // To not return the password even tho it is hashed
-        savedUser.password = undefined;
-        HttpManager.renderSuccess(res, { company: savedCompany, user: savedUser, token });
+            const user = await RegisterManager.createUser(
+                body.firstName,
+                body.lastName,
+                body.plainPassword,
+                body.mail,
+                body.phone,
+                body.profile,
+                savedCompany.id,
+            );
+            const savedUser = await user.save();
+
+            const token = jwt.sign(
+                { userId: savedUser.id },
+                process.env.RANDOM_TOKEN_SECRET,
+                { expiresIn: process.env.TOKEN_EXPIRATION_DELAY },
+            );
+            // To not return the password even tho it is hashed
+            savedUser.password = undefined;
+            HttpManager.renderSuccess(res, { company: savedCompany, user: savedUser, token });
+        } else {
+            HttpManager.renderError(res, new EmailAlreadyUserError());
+        }
     } catch (e) {
         HttpManager.renderError(res, e, e.code || 400);
     }
